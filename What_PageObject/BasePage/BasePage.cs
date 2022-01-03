9e40cfeeaafec70f02c487ccac3d070674f604ef
Sergey <Dnepr.IP@i.ua>
2022-01-03 17:13:37 +0200

    add locators

---
+++ What_PageObject/BasePage/BasePage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+
+namespace What_PageObject.BasePage
+{
+    public class BasePage
+    {
+        IWebDriver driver;
+
+        public BasePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void ClickElement(By locator)
+        {
+            driver.FindElement(locator).Click();
+        }
+
+        public void FillField(By locator, string text)
+        {
+            var field = driver.FindElement(locator);
+            field.Click();
+            field.SendKeys(text);
+        }
+    }
+}
---
+++ What_PageObject/Constants/Constant.cs
@@ -0,0 +1,33 @@
+﻿namespace WHAT_PageObject.Constants
+{
+    public static class Constant
+    {
+        public const string url = "http://localhost:8080";
+
+        #region Credentials
+
+        public const string adminLoginName = "james.smith@example.com";
+        public const string adminPasswordName = "Nj_PJ7K9";
+
+        public const string secretaryLoginName = "john.williams@example.com";
+        public const string secretaryPasswordName = "9mw6AJB_";
+
+        public const string mentorLoginName = "michael.taylor@example.com";
+        public const string mentorPasswordName = "9td6IO_Z";
+
+        public const string studentLoginName = "thomas.roberts@example.com";
+        public const string studentPasswordName = "T_oYiUX5";
+
+        #endregion
+
+        #region CredentialsExpectedResult
+
+        public const string expectedAdminResult = "Students";
+        public const string expectedSecretaryResult = "Mentors";
+        public const string expectedMentorResult = "Lessons";
+        public const string expectedStudentResult = "Courses";
+
+        #endregion
+
+    }
+}
