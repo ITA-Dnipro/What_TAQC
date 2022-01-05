058ef1151d882c3af45936d695732d914b0861b4
Artem Rozhkov <artem.rozhkov.1996@gmail.com>
2022-01-05 06:08:38 +0200

    Create project

---
+++ What_PageObject/DriverManager/DriverManager.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace What_PageObject.DriverManager
+{
+    internal class DriverManager
+    {
+        public DriverManager()
+        {
+
+        }
+
+        private static IWebDriver? driver;
+
+        public static IWebDriver Current
+        {
+            get
+            {
+                if (driver == null)
+                {
+                    driver = new ChromeDriver();
+                }
+                return driver;
+            }
+        }
+    }
+}
---
+++ What_PageObject/SignIn/SignInPage.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace What_PageObject.SignIn
+{
+    internal class SignInPage
+    {
+        public static readonly By signInButton = By.XPath(Locators.SignInButton);
+        public static readonly By emailField = By.XPath(Locators.EmailField);
+        public static readonly By passwordField = By.XPath(Locators.PasswordField);
+        public static readonly By registrationButton = By.XPath(Locators.RegistrationButton);
+        public static readonly By forgotPasswordButton = By.XPath(Locators.ForgotPasswordButton);
+
+        DriverManager.DriverManager Driver = new DriverManager.DriverManager();
+
+        private IWebElement signInButtonWebElement => DriverManager.DriverManager.Current.FindElement(By.XPath("//*[@class='btn button__default___3hOmG button__button___24ZfP auth__form-button___3KEpa']"));
+
+    }
+}
