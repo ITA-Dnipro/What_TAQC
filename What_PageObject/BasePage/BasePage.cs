6bf656e1cbce973eb8f38e64c6c8ce6fc85cc5fd
Sergey <Dnepr.IP@i.ua>
2022-01-08 14:39:56 +0200

    update basepage

--- What_PageObject/BasePage/BasePage.cs
+++ What_PageObject/BasePage/BasePage.cs
@@ -1,26 +1,27 @@
 ﻿using OpenQA.Selenium;
+using What_PageObject.UnassignedUsers;
 
 namespace What_PageObject.BasePage
 {
-    public class BasePage
+    public abstract class BasePage
     {
-        IWebDriver driver;
-
-        public BasePage(IWebDriver driver)
-        {
-            this.driver = driver;
-        }
-
         public void ClickElement(By locator)
         {
-            driver.FindElement(locator).Click();
+            DriverManager.DriverManager.Current.FindElement(locator).Click();
         }
 
         public void FillField(By locator, string text)
         {
-            var field = driver.FindElement(locator);
-            field.Click();
+            var field = DriverManager.DriverManager.Current.FindElement(locator);
+            field.SendKeys(Keys.Control + "a");
+            field.SendKeys(Keys.Delete);
             field.SendKeys(text);
+        }
+
+        public void Logout()
+        {
+            ClickElement(Locators.TopDropdownMenuButton);
+            ClickElement(Locators.TopDropdownLogoutButton);
         }
     }
 }
