6d5a8733cb01e7d358803e056259024e818e75a3
Aleksei Chikin <lehafar@gmail.com>
2022-01-11 19:14:54 +0200

    Add ResetPasswordPage

--- What_PageObject/BasePage.cs
+++ What_PageObject/BasePage.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
 
-namespace What_PageObject
+namespace PageObject
 {
-    internal class BasePage
+    public abstract class BasePage
     {
+        protected IWebDriver driver;
+
+        public BasePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        protected void FillField(By locator, string text)
+        {
+            var field = driver.FindElement(locator);
+            field.Click();
+            //field.Clear(); //??
+            field.SendKeys(Keys.Control + "a");
+            field.SendKeys(Keys.Delete);
+            field.SendKeys(text);
+        }
+        protected void ClickItem(By locator)
+        {
+            driver.FindElement(locator).Click();
+        }
     }
 }
