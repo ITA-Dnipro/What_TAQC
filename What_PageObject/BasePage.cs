a6e416455b861f7d34d91424a7cf865939844058
Aleksei Chikin <lehafar@gmail.com>
2022-01-12 17:32:08 +0200

    Returning of Base Page

---
+++ What_PageObject/BasePage.cs
@@ -0,0 +1,22 @@
+using OpenQA.Selenium;
+using What_Common.DriverManager;
+
+namespace What_PageObject.BasePage
+{
+    public abstract class BasePage
+    {
+        public void ClickElement(By locator)
+        {
+            Driver.Current.FindElement(locator).Click();
+            
+        }
+
+        public void FillField(By locator, string text)
+        {
+            var field = Driver.Current.FindElement(locator);
+            field.SendKeys(Keys.Control + "a");
+            field.SendKeys(Keys.Delete);
+            field.SendKeys(text);
+        }
+    }
+}
