221c8e570f5734945b0a72438f3cd0f7343a8a8a
Baday1010 <86745040+Baday1010@users.noreply.github.com>
2022-01-16 10:11:43 +0300

    Comment unworkable class

--- What_PageObject/ResetPasswordPage/ResetPasswordPage.cs
+++ What_PageObject/ResetPasswordPage/ResetPasswordPage.cs
@@ -1,39 +1,39 @@
-﻿using OpenQA.Selenium;
-using PageObject;
-using ResetPassword.Locators;
+﻿//using OpenQA.Selenium;
+//using What_PageObject;
+////using ResetPassword.Locators;
 
 
-namespace ResetPassword
-{
-    public class ResetPasswordPage : BasePage
-    {
-        public ResetPasswordPage(IWebDriver driver) : base(driver)
-        {
-        }
+//namespace ResetPassword
+//{
+//    public class ResetPasswordPage : BasePage
+//    {
+//        public ResetPasswordPage(IWebDriver driver) : base(driver)
+//        {
+//        }
 
-        public ResetPasswordPage FillEmail(string email)
-        {
-            FillField(ResetPasswordLocator.emailAddressField, email);
-            return this;
-        }
-        public ResetPasswordPage FillNewPassword(string password)
-        {
-            FillField(ResetPasswordLocator.newPasswordField, password);
-            return this;
-        }
-        public ResetPasswordPage FillConfirmPassword(string password)
-        {
-            FillField(ResetPasswordLocator.confirmPasswordField, password);
-            return this;
-        }
-        public ResetPasswordPage ClickConfirmButton()
-        {
-            ClickItem(ResetPasswordLocator.confirmButton);
-            return this;
-        }
-        public void ClickBackButton()
-        {
-            ClickItem(ResetPasswordLocator.backButton);
-        }
-    }
-}
+//        public ResetPasswordPage FillEmail(string email)
+//        {
+//            FillField(ResetPasswordLocator.emailAddressField, email);
+//            return this;
+//        }
+//        public ResetPasswordPage FillNewPassword(string password)
+//        {
+//            FillField(ResetPasswordLocator.newPasswordField, password);
+//            return this;
+//        }
+//        public ResetPasswordPage FillConfirmPassword(string password)
+//        {
+//            FillField(ResetPasswordLocator.confirmPasswordField, password);
+//            return this;
+//        }
+//        public ResetPasswordPage ClickConfirmButton()
+//        {
+//            ClickItem(ResetPasswordLocator.confirmButton);
+//            return this;
+//        }
+//        public void ClickBackButton()
+//        {
+//            ClickItem(ResetPasswordLocator.backButton);
+//        }
+//    }
+//}
