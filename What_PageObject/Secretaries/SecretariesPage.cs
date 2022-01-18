c3d79e12f52bf36a80c20dc000e8dbd20de30578
Sergey <Dnepr.IP@i.ua>
2022-01-18 16:48:54 +0200

    upd

--- What_PageObject/Secretaries/SecretariesPage.cs
+++ What_PageObject/Secretaries/SecretariesPage.cs
@@ -10,85 +10,85 @@ namespace What_PageObject.Secretaries
 {
     public class SecretariesPage : BasePage
     {
-        //        protected IWebDriver driver;
-        //        public SecretariesPage(IWebDriver driver)
-        //        {
-        //            this.driver = driver;
-        //        }
-        //        public SecretariesPage ClickNextTopButton()
-        //        {
-        //            ClickElement(Locators.ListOfSecretaryPage.paginationArrowRightInTop);
-        //            return this;
-        //        }
-        //        public SecretariesPage ClickPrevTopButton()
-        //        {
-        //            ClickElement(Locators.ListOfSecretaryPage.paginationArrowLeftInTop);
-        //            return this;
-        //        }
-        //        public SecretariesPage ClickNextBottomButton()
-        //        {
-        //            ClickElement(Locators.ListOfSecretaryPage.paginationArrowRightInTop);
-        //            return this;
-        //        }
-        //        public SecretariesPage ClickPrevBottomButton()
-        //        {
-        //            ClickElement(Locators.ListOfSecretaryPage.paginationArrowLeftInBottom);
-        //            return this;
-        //        }
-        //        public SecretariesPage ClickSearchField()
-        //        {
-        //            ClickElement(Locators.ListOfSecretaryPage.searchField);
-        //            return this;
-        //        }
-        //        public SecretariesPage FillSearchField(string text)
-        //        {
-        //            FillField(Locators.ListOfSecretaryPage.searchField, text);
-        //            return this;
-        //        }
-        //        public SecretaryDetailsPage ClickEditButtonInTable()
-        //        {
-        //            ClickElement(Locators.ListOfSecretaryPage.editButtonInTable);
-        //            return new SecretaryDetailsPage(driver);
-        //        }
-        //        public SecretaryDetailsPage ClickDetailsButtonInTable()
-        //        {
-        //            ClickElement(Locators.ListOfSecretaryPage.detailsButtonTable);
-        //            return new SecretaryDetailsPage(driver);
-        //        }
-        //        public SecretaryDetailsPage ClickEditButtonInCards()
-        //        {
-        //            ClickElement(Locators.ListOfSecretaryPage.editButtonInCards);
-        //            return new SecretaryDetailsPage(driver);
-        //        }
-        //        public SecretaryDetailsPage ClickDetailsButtonInCards()
-        //        {
-        //            ClickElement(Locators.ListOfSecretaryPage.detailsButtonCards);
-        //            return new SecretaryDetailsPage(driver);
-        //        }
-        //        public AddSecretaryPage ClickAddSecretaryButton()
-        //        {
-        //            ClickElement(Locators.ListOfSecretaryPage.addButton);
-        //            return new AddSecretaryPage(driver);
-        //        }
-        //        public SecretariesPage ClickSortByName()
-        //        {
-        //            ClickElement(Locators.ListOfSecretaryPage.sortByName);
-        //            return this;
-        //        }
-        //        public SecretariesPage ClickSortBySurname()
-        //        {
-        //            ClickElement(Locators.ListOfSecretaryPage.sortBySurname);
-        //            return this;
-        //        }
-        //        public SecretariesPage ClickSortByRmail()
-        //        {
-        //            ClickElement(Locators.ListOfSecretaryPage.sortByEmail);
-        //            return this;
-        //        }
-        //        public SecretariesPage ClickDisabledCheckbox()
-        //        {
-        //            ClickElement(Locators.ListOfSecretaryPage.disabledCheckbox);
-        //            return this;
-        //        }
+        protected IWebDriver driver;
+        public SecretariesPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        public SecretariesPage ClickNextTopButton()
+        {
+            ClickElement(Locators.ListOfSecretaryPage.paginationArrowRightInTop);
+            return this;
+        }
+        public SecretariesPage ClickPrevTopButton()
+        {
+            ClickElement(Locators.ListOfSecretaryPage.paginationArrowLeftInTop);
+            return this;
+        }
+        public SecretariesPage ClickNextBottomButton()
+        {
+            ClickElement(Locators.ListOfSecretaryPage.paginationArrowRightInTop);
+            return this;
+        }
+        public SecretariesPage ClickPrevBottomButton()
+        {
+            ClickElement(Locators.ListOfSecretaryPage.paginationArrowLeftInBottom);
+            return this;
+        }
+        public SecretariesPage ClickSearchField()
+        {
+            ClickElement(Locators.ListOfSecretaryPage.searchField);
+            return this;
+        }
+        public SecretariesPage FillSearchField(string text)
+        {
+            FillField(Locators.ListOfSecretaryPage.searchField, text);
+            return this;
+        }
+        public SecretaryDetailsPage ClickEditButtonInTable()
+        {
+            ClickElement(Locators.ListOfSecretaryPage.editButtonInTable);
+            return new SecretaryDetailsPage(driver);
+        }
+        public SecretaryDetailsPage ClickDetailsButtonInTable()
+        {
+            ClickElement(Locators.ListOfSecretaryPage.detailsButtonTable);
+            return new SecretaryDetailsPage(driver);
+        }
+        public SecretaryDetailsPage ClickEditButtonInCards()
+        {
+            ClickElement(Locators.ListOfSecretaryPage.editButtonInCards);
+            return new SecretaryDetailsPage(driver);
+        }
+        public SecretaryDetailsPage ClickDetailsButtonInCards()
+        {
+            ClickElement(Locators.ListOfSecretaryPage.detailsButtonCards);
+            return new SecretaryDetailsPage(driver);
+        }
+        public AddSecretaryPage ClickAddSecretaryButton()
+        {
+            ClickElement(Locators.ListOfSecretaryPage.addButton);
+            return new AddSecretaryPage(driver);
+        }
+        public SecretariesPage ClickSortByName()
+        {
+            ClickElement(Locators.ListOfSecretaryPage.sortByName);
+            return this;
+        }
+        public SecretariesPage ClickSortBySurname()
+        {
+            ClickElement(Locators.ListOfSecretaryPage.sortBySurname);
+            return this;
+        }
+        public SecretariesPage ClickSortByRmail()
+        {
+            ClickElement(Locators.ListOfSecretaryPage.sortByEmail);
+            return this;
+        }
+        public SecretariesPage ClickDisabledCheckbox()
+        {
+            ClickElement(Locators.ListOfSecretaryPage.disabledCheckbox);
+            return this;
+        }
     }
 }
