ab412c846cbeb94ba75f25bdb6b7d4c490fcf98a
Baday1010 <86745040+Baday1010@users.noreply.github.com>
2022-01-31 20:05:31 +0300

    Add course tests

---
+++ What_APITest/API_Tests/CoursesTests/GetAllCoursesTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Allure.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using What_APIObject.Entities.Accounts;
+using What_APIObject.Objects.Courses;
+using What_Common.DataProvider;
+
+namespace What_APITest.API_Tests.CoursesTests
+{
+    [AllureNUnit]
+    internal class GetAllCoursesTest : BaseTest
+    {
+        CourseObject courseObject;
+        LoginDetails admin = Controller.GetUser(Controller.UserRole.Admin);
+
+        [SetUp]
+        public void Setup()
+        {
+            courseObject = new CourseObject(new User
+            {
+                Email = admin.Email,
+                Password = admin.Password,
+                Role = Controller.UserRole.Admin.ToString().ToLower()
+            });
+            courseObject
+                .RegistrationNewUser();
+
+        }
+
+        [Test(Description = "XXX")]
+        public void VerifyThatAllCoursesGet()
+        {
+            courseObject.GetAllCourses();
+        }
+
+        [TearDown]
+        public void After()
+        {
+            
+        }
+    }
+}
