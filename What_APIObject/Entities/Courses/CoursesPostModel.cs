4c4ed4de273082963dcbc304826e74ed98791c8b
Baday1010 <86745040+Baday1010@users.noreply.github.com>
2022-01-31 20:04:46 +0300

    Add new models

---
+++ What_APIObject/Entities/Courses/CoursesPostModel.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace What_APIObject.Entities.Courses
+{
+    public class CoursesPostModel
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+    }
+}
